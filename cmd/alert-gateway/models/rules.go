5ea96cd1230614f43bdb0627aa4beaa16ff045bf
jayryu <56525134+jayryu@users.noreply.github.com>
2020-05-28 10:34:56 +0800

    Update rules.go

--- cmd/alert-gateway/models/rules.go
+++ cmd/alert-gateway/models/rules.go
@@ -13,7 +13,7 @@ type Rules struct {
 	Id          int64  `orm:"column(id);auto" json:"id,omitempty"`
 	Expr        string `orm:"column(expr);size(1023)" json:"expr"`
 	Op          string `orm:"column(op);size(31)" json:"op"`
-	Value       string `orm:"column(value);size(31)" json:"op"`
+	Value       string `orm:"column(value);size(1023)" json:"op"`
 	For         string `orm:"column(for);size(1023)" json:"for"`
 	Summary     string `orm:"column(summary);size(1023)" json:"summary"`
 	Description string `orm:"column(description);size(1023)" json:"description"`
